f2b12fff824f3920c0d3a4d540edd93ed5a0fe15
Zachary Labelle <Zac.Labelle@Hotmail.com>
2021-03-18 18:12:41 -0400

    Création test3

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -37,4 +37,10 @@ describe('ProblemeComponent', () => {
     expect(zone.valid).toBeTruthy();
   });
 
+  it('#3 | Zone PRÉNOM valide avec 200 caractères', () => {
+    let zone = component.problemeForm.get('prenom');
+    zone.setValue('a'.repeat(200));
+    expect(zone.valid).toBeTruthy();
+  });
+
 });
