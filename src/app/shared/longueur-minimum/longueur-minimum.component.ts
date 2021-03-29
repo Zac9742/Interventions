1c3157119994bbae7a2c46415c85944c008f517b
Zachary Labelle <Zac.Labelle@Hotmail.com>
2021-03-29 12:25:22 -0400

    Fin tp9

--- src/app/shared/longueur-minimum/longueur-minimum.component.ts
+++ src/app/shared/longueur-minimum/longueur-minimum.component.ts
@@ -13,4 +13,16 @@ export class ZonesValidator {
             return { 'nbreCaracteresInsuffisants': true };
         };
     }
+    static longueurMaximum(longueur: number): ValidatorFn {
+        // Sous ANGULAR dans les validateurs pour indiquer un succès retourner NULL autrement retourner une clé valeur JSON
+        return (controle: AbstractControl): { [key: string]: boolean} | null => {
+            if (controle.value == null || controle.value.trim().isEmpty){
+                return { 'nbreCaracteresDepasse': true };
+            }
+            if (controle.value.trim().length <= longueur) {
+                return null;
+            }
+            return { 'nbreCaracteresInsuffisants': true };
+        };
+    }
 }
